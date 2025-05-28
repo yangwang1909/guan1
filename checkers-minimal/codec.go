19e4b6572b2921349a784a4ddb32e3974a9f742c
Yang <yang.wang1909@gmail.com>
2025-05-28 05:23:09 +0000

    first commit

---
+++ checkers-minimal/codec.go
@@ -0,0 +1,10 @@
+package checkers
+
+import (
+    types "github.com/cosmos/cosmos-sdk/codec/types"
+)
+
+// RegisterInterfaces registers the interfaces types with the interface registry.
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+}
+
---
+++ checkers-minimal/keys.go
@@ -0,0 +1,10 @@
+package checkers
+
+import "cosmossdk.io/collections"
+
+const ModuleName = "checkers"
+
+var (
+    ParamsKey  = collections.NewPrefix("Params")
+)
+
